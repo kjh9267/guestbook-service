5864949fb752ba30c45ba8bc47af14daeb675ef9
kjh9267 <intellijunho@kakao.com>
2024-09-28 05:16:22 +0900

    feat: Apply timed in controller methods

--- src/main/java/me/jun/guestbookservice/core/presentation/PostController.java
+++ src/main/java/me/jun/guestbookservice/core/presentation/PostController.java
@@ -1,5 +1,6 @@
 package me.jun.guestbookservice.core.presentation;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.jun.guestbookservice.common.security.WriterId;
@@ -28,6 +29,10 @@ public class PostController {
             produces = APPLICATION_JSON,
             consumes = APPLICATION_JSON
     )
+    @Timed(
+            value = "posts.create",
+            longTask = true
+    )
     public Mono<ResponseEntity<PostResponse>> createPost(@RequestBody @Valid CreatePostRequest request, @WriterId Long writerId) {
         Mono<CreatePostRequest> requestMono = Mono.fromSupplier(
                 () -> request.toBuilder()
@@ -47,6 +52,10 @@ public class PostController {
             value = "/{postId}",
             produces = APPLICATION_JSON
     )
+    @Timed(
+            value = "posts.retrieve",
+            longTask = true
+    )
     public Mono<ResponseEntity<PostResponse>> retrievePost(@PathVariable Long postId) {
         Mono<RetrievePostRequest> requestMono = Mono.fromSupplier(
                 () -> RetrievePostRequest.of(postId)
@@ -64,6 +73,10 @@ public class PostController {
             consumes = APPLICATION_JSON,
             produces = APPLICATION_JSON
     )
+    @Timed(
+            value = "posts.update",
+            longTask = true
+    )
     public Mono<ResponseEntity<PostResponse>> updatePost(@RequestBody @Valid UpdatePostRequest request, @WriterId Long writerId) {
         Mono<UpdatePostRequest> requestMono = Mono.fromSupplier(
                 () -> request.toBuilder()
@@ -80,6 +93,10 @@ public class PostController {
     }
 
     @DeleteMapping("/{postId}")
+    @Timed(
+            value = "posts.delete",
+            longTask = true
+    )
     public Mono<ResponseEntity<Void>> deletePost(@PathVariable Long postId, @WriterId Long writerId) {
         Mono<DeletePostRequest> requestMono = Mono.fromSupplier(
                 () -> DeletePostRequest.builder()
@@ -98,6 +115,10 @@ public class PostController {
     @GetMapping(
             value = "/query",
             produces = APPLICATION_JSON_VALUE
+    )
+    @Timed(
+            value = "posts.retrieveList",
+            longTask = true
     )
     public Mono<ResponseEntity<PostListResponse>> retrievePostList(
             @RequestParam("page") int page,
