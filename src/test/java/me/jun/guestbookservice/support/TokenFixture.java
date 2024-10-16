120d0c52d36f1b71602bb702bbba76c2da663478
kjh9267 <intellijunho@kakao.com>
2024-10-17 02:49:21 +0900

    test: Add expiration in jwt

--- src/test/java/me/jun/guestbookservice/support/TokenFixture.java
+++ src/test/java/me/jun/guestbookservice/support/TokenFixture.java
@@ -1,14 +1,26 @@
 package me.jun.guestbookservice.support;
 
+import io.jsonwebtoken.Jwts;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import static io.jsonwebtoken.SignatureAlgorithm.HS512;
+import static java.time.Instant.now;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 abstract public class TokenFixture {
 
-    public static final String JWT_KEY = "bUM2q6grP497hK4NKzCFbM9X8EBFbLgXXQXqmdShvNlkHNbQkk460rELLYxVA4cU+0rg9AIIl4ReodO5LKm2j+vlT+z844EznNTQzSEigdnGeE8E+KQNk4vmsBEqg8s4VJLSyv+EyadfrZMtmArlScxQaK69cmwppQ4Fv3hkB+U=";
+    public static final String JWT_KEY = "1gnP47fOh0meRrSkU4n3ujd/CzNpGV3W9mBbgul+diJrQicNRavTYO5gYqluS/vsftHwyKZOFFtULf9cFVGsRuBG0ochG2LCHLuOuKvjkBJMz4xhimH+auDSA+jCdnuPyig6Ak6mF1u1Ovyck68Uc0qsebioLe7rpnF9lhPgCN4=";
 
-    public static final String TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIn0.6ay5LRrQAzHAxXepa18xAOzlfcPZn2WAk4KRTt__bL7qk-IaaFBp5Y5OECNF1hoJip7Kl0MNWxRoqA1s4YjupA";
-
-    public static final String EMAIL = "asdf@asdf.com";
+    public static String createToken(Long id, Long expirationMinutes) {
+        return Jwts.builder()
+                .subject(id.toString())
+                .issuedAt(Date.from(now()))
+                .expiration(Date.from(now().plus(expirationMinutes, MINUTES)))
+                .signWith(HS512, JWT_KEY)
+                .compact();
+    }
 }
