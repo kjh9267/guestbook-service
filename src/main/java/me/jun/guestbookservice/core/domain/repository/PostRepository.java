d0b8f22e3c4ba103b4b711ee6ee4148d468a90da
kjh9267 <intellijunho@kakao.com>
2024-10-19 22:24:36 +0900

    feat: Create deleteAllByWriter in postRepository -test: Create deleteAllByWriterTest in postRepository test

--- src/main/java/me/jun/guestbookservice/core/domain/repository/PostRepository.java
+++ src/main/java/me/jun/guestbookservice/core/domain/repository/PostRepository.java
@@ -1,12 +1,19 @@
 package me.jun.guestbookservice.core.domain.repository;
 
 import me.jun.guestbookservice.core.domain.Post;
+import me.jun.guestbookservice.core.domain.Writer;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllBy(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Post p where p.writer = :writer")
+    void deleteAllByWriter(Writer writer);
 }
--- src/test/java/me/jun/guestbookservice/core/domain/repository/PostRepositoryTest.java
+++ src/test/java/me/jun/guestbookservice/core/domain/repository/PostRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static me.jun.guestbookservice.support.PostFixture.post;
+import static me.jun.guestbookservice.support.WriterFixture.writer;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ActiveProfiles("test")
@@ -31,5 +32,23 @@ class PostRepositoryTest {
 
         assertThat(postRepository.findAllBy(PageRequest.of(0, 10)).size())
                 .isEqualTo(expected);
+    }
+
+    @Test
+    void deleteAllByWriterTest() {
+        for (long id = 1; id <= 10; id++) {
+            Post post = post().toBuilder()
+                    .id(id)
+                    .build();
+
+            postRepository.save(post);
+        }
+
+        postRepository.deleteAllByWriter(writer());
+
+        for (long id = 1; id <= 10; id++) {
+            assertThat(postRepository.findById(id))
+                    .isEmpty();
+        }
     }
 }
